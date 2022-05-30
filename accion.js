07bb49a8e651a794ab3f4620e5400cb0a32c2c1d
Altaskur <105789412+altaskur@users.noreply.github.com>
2022-05-30 22:25:33 +0200

    Segundo Directo
    Optimizada y reescrita la aplicación usando GitHub Copilot

--- accion.js
+++ accion.js
@@ -1,181 +1,93 @@
-// Dónde se guardarán los ciclos del reloj
-var ciclo = 0;
+// constantes de tiempo [Minutos]
 
-// Dónde se guardará si el reloj esta [Descanso, Contando, Parado]
-var estaParado = true;
-var tiempo = 0;
-var descanso = false;
-var totalCiclos = 0;
+tiempoTrabajo = 25 * 60;
+tiempoDescanso = 5 * 60;
+tiempoDescansoLargo = 10 * 60;
+activarDescansoLargo = 2; // Número de ciclos para activar el descanso largo.
 
-var tiempoTrabajado = 0;
-var totalDescansos = 0;
-var tiempoDescanso = 0;
+//  variables control
+ciclos = 0;
+estaParado = true;
+descanso = false;
+totalCiclos = 0;
+tiempo = 0;
+tiempoRestante = 0;
+estado = "";
 
-// cantidad de ciclos para hacer una Descanso Largo
-var cicloDescansoLargo = 2;
+// Capturas del html para mostrar el tiempo
+pomodoroContainer = document.getElementById("pomodoro");
+pomodoroReloj = document.getElementById("reloj");
+pomodoroTexto = document.getElementById("texto");
+pomodoroSonido = document.querySelector("audio");
 
-// OPCIONES DEL RELOJ en minutos \\
-var duracionCiclo = 25;
-var duracionDescanso = 5;
-var duracionDescansoLargo = 10;
-
-// Pasamos de minutos a milisegundos.
-
-duracionCiclo = duracionCiclo * 60;
-duracionDescanso = duracionDescanso * 60;
-duracionDescansoLargo = duracionDescansoLargo * 60;
-
-// duracion alarma
-var duracionAlarma = 5;
-
-// Capturamos los contenedores del pomodoro
-let pomodoroReloj = document.getElementById("reloj");
-let pomodoroContainer = document.getElementById("pomodoro");
-let pomodoroEstado = document.getElementById("estado")
-
-
+// Eventos del pomodoro
 pomodoroContainer.addEventListener("click", () => {
-    estaParado == true ? estaParado = false : estaParado = true;
-    estaParado == true ? pomodoroEstado.textContent = "Estoy Parado" : " ";
-    console.log("Me has parado")
+    estaParado === true ? (estaParado = false, empezarContar()) : (estaParado = true, pomodoroTexto = "estoy parado", dejarContar());
+
 });
 
-
-// Sonidos del pomodoro
-
-var alarma = document.querySelector("audio");
-
-
-var t = window.setInterval(function () {
-
-    console.log("¿estoy parado? " + estaParado)
-
-    // Comprobamos si está el reloj en Descanso
-    if (!estaParado) {
-
-        // sumamos +1s al contador
+function empezarContar() {
+    contador = window.setInterval(() => {
+        // Empezamos a contar
         tiempo++;
 
-        if (tiempo == duracionCiclo && !descanso) {
 
-            // si llega al fin +1c y reseteamos el contador
-            ciclo++
-            totalCiclos++;
-            tiempo = 0;
-            descanso = true;
-        }
+        // Comprobamos si hay un descanso en marcha
+        if (descanso === true) {
 
-        if (descanso) {
+            // comprobamos el tipo de descanso
+            if (ciclos == activarDescansoLargo) {
 
-            // Añadimos los datos del tiempo de descanso
-            tiempoDescanso++;
-
-            // hacemos sonar la alarma durante 3 segundos
-            // cada vez que entremos en un descanso
-            if (tiempo <= duracionAlarma) {
-                alarma.play();
-            }
-
-
-            // Comprobamos en que tipo de descanso estamos
-
-            if (ciclo == cicloDescansoLargo) {
-                // Si estamos en el descanso largo
-
-                // cambiamos el estilo del display
-                // Mostrar el tiempo que falta en el display
-                pomodoroReloj.classList.add("descansoLargo");
-                pomodoroReloj.textContent = (duracionDescansoLargo - tiempo);
-
-
-                let restanteDescansoLargoMin = (duracionDescansoLargo - tiempo) / 60;
-                restanteDescansoLargoMin < 10 ?  restanteDescansoLargoMin = "0" + Math.round(restanteDescansoLargoMin) : restanteDescansoLargoMin = Math.round(restanteDescansoLargoMin);
-
-                let restanteDescansoLargoSec = (duracionDescansoLargo - tiempo) % 60;
-                restanteDescansoLargoSec < 10 ? restanteDescansoLargoSec = "0" + restanteDescansoLargoSec : restanteDescansoLargoSec = restanteDescansoLargoSec;
-
-                pomodoroReloj.textContent = restanteDescansoLargoMin + " : " + restanteDescansoLargoSec;
-
-                pomodoroEstado.textContent = "¡Descanso largo!";
-
-                // Si ha acabado el tiempo del descanso largo reseteamos el tiempo
-                // Y salimos del descanso
-                if (tiempo == duracionDescansoLargo) {
-                    tiempo = 0;
-                    descanso = false;
-                }
+                // Entramos en descanso largo
+                // Comprobamos que no se haya acabado el tiempo
+                // reseteando los ciclos, el tiempo, el tiempo restante y el descanso
+                // sino añadimos el tiempo restante.
+                tiempo === tiempoDescansoLargo ? (descanso = false, ciclos = 0, tiempo = 0, tiempoRestante = 0) : tiempoRestante = (tiempoDescansoLargo - tiempo);
+                
+                // Añadimos el estado y la clase de CSS
+                estado = "Descanso largo";
+                pomodoroReloj.classList.add("descanso-largo");
 
             } else {
-                // Si estamos en el descanso corto 
-                // añadimos el estilo al display de descanso corto
-                // mostrar cuanto queda del descanso corto
-                pomodoroReloj.classList.add("descanso");
 
-                pomodoroEstado.textContent = "¡Descanso corto!";
+                // Entramos en descanso corto
+                // Comprobamos que no se haya acabado el tiempo
+                // reseteando los ciclos, el tiempo, el tiempo restante y el descanso
+                // sino añadimos el tiempo restante.
+                tiempo === tiempoDescanso ? (descanso = false, tiempo = 0, tiempoRestante = 0) : tiempoRestante = (tiempoDescanso - tiempo);
 
-                let restanteDescansoCotoMin = (duracionDescanso - tiempo) / 60;
-                restanteDescansoCotoMin < 60 ? restanteDescansoCotoMin = 0 : "";
-                restanteDescansoCotoMin < 10 ? restanteDescansoCotoMin = "0" + Math.floor(restanteDescansoCotoMin) : restanteDescansoCotoMin = Math.floor(restanteDescansoCotoMin)
-
-                let restanteDescansoCotoSec = (duracionDescanso - tiempo) % 60;
-                 restanteDescansoCotoSec < 10 ? restanteDescansoCotoSec = "0" + restanteDescansoCotoSec : restanteDescansoCotoSec = restanteDescansoCotoSec
-
-                pomodoroReloj.textContent = restanteDescansoCotoMin + " : " + restanteDescansoCotoSec;
-
-
-                // Si ha acabado el tiempo del descanso corto reseteamos el tiempo
-                // Y salimos del descanso
-                if (tiempo == duracionDescanso) {
-                    tiempo = 0;
-                    descanso = false;
-                }
-
-
+                // Añadimos el estado y la clase de CSS
+                estado = "Descanso corto";
+                pomodoroReloj.classList.add("descanso-corto");
             }
+
 
         } else {
-            // si entramos en el modo trabajo 
-            // hacemos sonar la alarma durante 3 segundos
+            // Entramos en tiempo de trabajo.
+            // reseteando los ciclos, el tiempo, el tiempo restante y el descanso
+            // sino añadimos el tiempo restante.
+            tiempo === tiempoTrabajo ? (descanso = true, tiempo = 0, ciclos++, tiempoRestante = 0) : tiempoRestante = (tiempoTrabajo - tiempo);
 
-            if (tiempo <= duracionAlarma) {
-                alarma.play();
-            }
-
-            // añadimos los datos del tiempo de trabajo
-            tiempoTrabajado++;
-
-            // al entrar al tiempo de trabajo 
-            // quitamos los estilos de descanso
-            // y mostramos el tiempo de trabajo que queda
-
-            pomodoroReloj.classList.remove("descanso");
-            pomodoroReloj.classList.remove("descansoLargo");
-
-            // Mostramos los minutos
-            let restanteTrabajoMin = (duracionCiclo - tiempo) / 60;
-
-            // Mostramos los minutos teniendo en cuenta el formato 00
-            restanteTrabajoMin = restanteTrabajoMin < 10 ? "0" + Math.floor(restanteTrabajoMin) : Math.floor(restanteTrabajoMin)
-
-            let restanteTrabajoSec = (duracionCiclo - tiempo) % 60;
-
-            // Mostramos los segundos teniendo en cuenta el formato 00
-            restanteTrabajoSec = restanteTrabajoSec < 10 ? "0" + restanteTrabajoSec : restanteTrabajoSec
-
-            console.log(restanteTrabajoMin)
-
-            console.log(restanteTrabajoSec)
-
-            pomodoroReloj.textContent = restanteTrabajoMin + " : " + restanteTrabajoSec;
-
-            pomodoroEstado.textContent = "¡A trabajar!";
-
+            // mostramos el estado del pomodoro
+            estado = "Trabajando";
+            pomodoroReloj.classList.remove("descanso-largo");
+            pomodoroReloj.classList.remove("descanso-corto");
         }
 
-        console.log("Estamos en el ciclo " + ciclo)
-        console.log("Tiempo transcurrido " + tiempo)
-    }
+        // Comprobamos el tiempo restante para activar la alarma
+        // Sonará 3 veces, una vez por cada minuto * uno de silencio.
+        tiempoRestante != 0 && tiempoRestante <= 6 ? (pomodoroSonido.play(), pomodoroReloj.classList.add("test")): (pomodoroSonido.pause(),  pomodoroReloj.classList.remove("test"));
 
+        // Mostramos el tiempo restante
+        // sacamos minutos y segundos
+        minutos = Math.floor(tiempoRestante / 60)
+        segundos = tiempoRestante % 60;
+        pomodoroReloj.textContent = (minutos < 10 ? "0" + minutos : minutos) + ":" + (segundos < 10 ? "0" + segundos : segundos);
+        pomodoroTexto.textContent = estado;
 
+    }, 1000);
+}
 
-}, 1000);
+function dejarContar() {
+    window.clearTimeout(contador);
+}
